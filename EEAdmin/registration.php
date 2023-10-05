e8137ee61a95c42010c0f4b0abcd72bc9a1d692d
Jean-Michel Batty <jean-michel.batty@extend.com>
2023-10-05 15:46:52 -0500

    original commit.
    This module requires Extend's Warranty module (https://github.com/helloextend/magento-extension/) and magento Enterprise Edition
    
    Make sure to drop the contents in app/code/Extend
    you will end up with app/code/Extend/EEAdmin
    
    This add-on fixes the missing "Add Products By SKU" in the admin / orders / create new order

---
+++ EEAdmin/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Project: Magento2 EE "Add Products By Sku"
+ * Date: 10/05/23
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Extend_EEAdmin',
+    __DIR__
+);
